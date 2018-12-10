6e2531cedae6154cf79d57087765f2b463469c17
yewenxiang <yewenxiang23@gmail.com>
2018-12-10 22:34:18 +0800

    Update data.js
    修改图片路径

--- src/blogData/data.js
+++ src/blogData/data.js
@@ -2,41 +2,41 @@ export const productData = [
     {
       'title': 'App后台管理系统',
       'desc': '使用vue vue-router完成的一个管理系统，包含登录、权限管理、文章编辑，视频上传，图片上传，等功能。',
-      'imgName':'http://ozrm3516s.bkt.clouddn.com/8edc9b043108a7713df1b335f93a5ac1.jpg',
+      'imgName':'http://wx4.sinaimg.cn/large/0073tXM5gy1fy1ye2jtcbj30zk0m8tck.jpg',
       'demoAddress': null,
       'codeAddress': null
     },
     {
       'title': '顽童星球APP下载页',
       'desc': '使用jq制作的一个APP官方下载页面，PC端和移动端。',
-      'imgName':'http://ozrm3516s.bkt.clouddn.com/72db99ac79e12b741b3a55cf69597818.jpg',
+      'imgName':'http://wx4.sinaimg.cn/large/0073tXM5gy1fy1ye2yvs8j30zk0m879l.jpg',
       'demoAddress': 'https://wtplanet.com/'
     },
     {
         'title': '刷粉神器',
         'desc': '使用react-native开发的app，耗时半个月完成。APP主要功能是快手用户刷粉刷评论刷播放下单的APP，整体和上个APP类似，登录使用微信和QQ登录，支付功能有支付宝和微信，APP后台系统也是个人独立开发，使用了React + ICE来开发后台单页面系统，项目目前准备上线。',
-        'imgName': 'http://ozrm3516s.bkt.clouddn.com/4b98d8a75632cfd736da2242cbaa33b2.jpg',
+        'imgName': 'http://wx1.sinaimg.cn/large/0073tXM5gy1fy1ye3jddvj31hc0u0wt0.jpg',
         'demoAddress': null,
         'codeAddress': null
     },
     {
         'title': '直播间抽奖',
         'desc': '使用webpack + jquery + bootstrap 来开发的一个直接间抽奖页面，使用websocket 实时输出弹幕，获取用户，并且在发送的弹幕用户中中抽奖。',
-        'imgName': 'http://ozrm3516s.bkt.clouddn.com/bad0bf630aa063543c07b7fdf1d9cfdd.jpg',
+        'imgName': 'http://wx1.sinaimg.cn/large/0073tXM5gy1fy1ye39j0dj31hc0u0ag7.jpg',
         'demoAddress': 'https://yewenxiang23.github.io/ksTakeAwardWebpack/dist/',
         'codeAddress':null
     },
     {
         'title': '变量管理系统',
         'desc':'使用react开发的后台管理系统，组件方面使用了阿里ICE框架，主要用来保存项目的公共代码，和使用JSON数据来生成表单，表单中填写数据，生成api，来提高项目的开发效率。',
-        'imgName': 'http://ozrm3516s.bkt.clouddn.com/46a6815187a5053909a2a07fff0bba03.jpg',
+        'imgName': 'http://wx4.sinaimg.cn/large/0073tXM5gy1fy1ye2r2anj310k0kzach.jpg',
         'demoAddress': null,
         'codeAddress': null
     },
     {
         'title': '快手网红联盟',
         'desc': '一个刷粉项目的APP，目前只上线了android版本，耗时一个多月完成。APP主要功能是快手用户互粉互赞互评，互粉功能使用websocket技术完成，整体使用react-navigation做路由，使用redux来做全局状态的管理，包含微信支付宝支付功能等。android版本已经上线，目前总用户量8W+，日活跃8k+。',
-        'imgName': 'http://ozrm3516s.bkt.clouddn.com/8dd4acbf1e23161a59eac031f2d47d29.jpg',
+        'imgName': 'http://wx4.sinaimg.cn/large/0073tXM5gy1fy1ye2cs4mj30zk0m80yp.jpg',
         'demoAddress': 'http://ksht-static.3agzs.com/download/rmhz_v1.5.0.apk',
         'codeAddress': null
         
@@ -44,9 +44,9 @@ export const productData = [
     {
         'title': 'ToyDB',
         'desc': '一个玩具测评平台的APP,主要使用React-native技术来开发出适配ios和android两端的App应用,即将上线。App项目从头到尾都是自己一个人开发的，5底月进公司，边学react-native边做项目，耗时两个月左右完成了这个项目，现阶段是界面的一些改版，数据的补充，项目中使用了redux来实现，数据的统一管理。使用coding来做版本管理，，项目包含登录模块，评论模块，文章模块，个人中心模块，视屏播放模块，搜索模块。',
-        'imgName': 'app-img_01',
+        'imgName': '![image](https://wx2.sinaimg.cn/large/0073tXM5gy1fy20uuzl89j30qm0ve4jy.jpg)',
         'demoAddress': 'http://fir.im/tmcj',
         'codeAddress': null
     },
-    { 'title': '云美丽', 'desc': '一个电商网站项目，总共包含六个子页面，含首页、列表页、详情页、支付页、注册页、登录页。并且包含基本的逻辑功能，购物车、轮播、标签页等等都是使用原生JS来开发的。', 'imgName': 'yunmeili', 'demoAddress': 'yunmeili', 'codeAddress': 'yunmeili' },
+    { 'title': '云美丽', 'desc': '一个电商网站项目，总共包含六个子页面，含首页、列表页、详情页、支付页、注册页、登录页。并且包含基本的逻辑功能，购物车、轮播、标签页等等都是使用原生JS来开发的。', 'imgName': 'https://ws1.sinaimg.cn/large/0073tXM5gy1fy20vmjk36j30zk0m8ng2.jpg', 'demoAddress': 'yunmeili', 'codeAddress': 'yunmeili' },
 ]
